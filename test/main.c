4ce0baf905244d77871ab60c67fdf6885f9b0c10
Shekhar Deo <deo@unknown68a86d3f805a.gateway.2wire.net>
2012-09-25 19:05:44 -0700

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test
+//
+//  Created by Shekhar Deo on 9/25/12.
+//  Copyright (c) 2012 Shekhar Deo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
